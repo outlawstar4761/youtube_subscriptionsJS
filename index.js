950136ebc33223c26c48ac0f20b1238eb49b8982
John J. Watson <outlawstar4761@gmail.com>
2021-06-22 22:24:09 +0100

    testing

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const google = require('./src/googleModule');
+const youtube = require('./src/youTubeDlModule');
 
 (async ()=>{
-  let secret = google.getSecret();
-  google.authorize(secret,async (auth)=>{
-    let subs = await google.getSubscriptions(auth,null,[]);
-    let channelIds = subs.map((sub)=>{return sub.snippet.resourceId.channelId});
-    channelIds.forEach(async (id)=>{
-      let activity = await google.getChannelActivity(auth,id);
-      console.log(activity.items[0].contentDetails);
-    });
-  });
+  // let secret = google.getSecret();
+  // google.authorize(secret,async (auth)=>{
+  //   let subs = await google.getSubscriptions(auth,null,[]);
+  //   let channelIds = subs.map((sub)=>{return sub.snippet.resourceId.channelId});
+  //   channelIds.forEach(async (id)=>{
+  //     let activity = await google.getChannelActivity(auth,id);
+  //     console.log(activity.items[0].contentDetails);
+  //   });
+  // });
+  let output = await youtube.download("7E-cwdnsiow");
+
 })();
 
 // channel.pageInfo.resultsPage
